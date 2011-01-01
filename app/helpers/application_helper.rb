06b8520e81afde41443a108b77780efaca265fc5
Atsushi Okatsu <mellow.moonlight@gmail.com>
2011-01-02 00:31:16 +0900

    Chapter 5 Exercises done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,9 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  
+  # Returns an logo image html
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
 end
